733ed7c4ee13a1fef0aa63a5f980ae3c59d19d51
rimmugyr <rimmugygr@gmail.com>
2020-07-17 11:50:42 +0200

    project course fix n+1 problem in tasks repo

--- Course-SpringMVCBootstrap/src/main/java/com/example/repository/TaskRepo.java
+++ Course-SpringMVCBootstrap/src/main/java/com/example/repository/TaskRepo.java
@@ -4,6 +4,8 @@ import com.example.model.Level;
 import com.example.model.Task;
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,10 @@ import java.util.List;
 @Repository
 public interface TaskRepo extends JpaRepository<Task,Long> {
 
-    List<Task> findAllByLevel(Level level);
+    @Override
+    @Query("select distinct t from Task t join fetch t.person")
+    List<Task> findAll();
+
+    @Query("select distinct t from Task t join fetch t.person WHERE t.level = :level")
+    List<Task> findAllByLevel(@Param("level") Level level);
 }
